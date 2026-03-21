722ba503d99e7b02f5253b4db919e7246a026605
manuelduarte077 <carlosd99m@gmail.com>
2026-03-21 01:26:00 -0600

    fix(homebrew): sha256 for git-flow-cli-2.0.1.tgz from release

--- Formula/git-flow-cli.rb
+++ Formula/git-flow-cli.rb
@@ -2,7 +2,7 @@ class GitFlowCli < Formula
   desc "CLI para crear ramas y commits con convención GitBN"
   homepage "https://github.com/manuelduarte077/git-flow-cli"
   url "https://github.com/manuelduarte077/git-flow-cli/releases/download/v2.0.1/git-flow-cli-2.0.1.tgz"
-  sha256 "3fd4d421a634f2207209570bfdfdaf487642393298ac429700aa1e36f4dd9df6"
+  sha256 "1b18fc3e8e132d45b4d0bb0b8894b56daa497953b3343af458ea962602e50d01"
   license "MIT"
 
   depends_on "openjdk@21"
