d892c9b515e3b6a07a67846a46e7d318029453d8
manuelduarte077 <carlosd99m@gmail.com>
2026-03-23 22:42:04 -0600

    chore(homebrew): bump formula to v2.0.2

--- Formula/git-flow-cli.rb
+++ Formula/git-flow-cli.rb
@@ -1,8 +1,8 @@
 class GitFlowCli < Formula
   desc "CLI para crear ramas y commits con convención GitBN"
   homepage "https://github.com/manuelduarte077/git-flow-cli"
-  url "https://github.com/manuelduarte077/git-flow-cli/releases/download/v2.0.1/git-flow-cli-2.0.1.tgz"
-  sha256 "1b18fc3e8e132d45b4d0bb0b8894b56daa497953b3343af458ea962602e50d01"
+  url "https://github.com/manuelduarte077/git-flow-cli/releases/download/v2.0.2/git-flow-cli-2.0.2.tgz"
+  sha256 "8e9ecca4cee1a720c6daa9ff0c595c04492b86da2f6345f11c06c5be3d55529d"
   license "MIT"
 
   depends_on "openjdk@21"
